1dbfeeedfabff0172b2b4267d7879d7844d1d5f9
Pawan Gupta <pawan-crestron>
2022-10-08 20:50:17 +0530

    First commit.

---
+++ src/main/java/com/wittgroup/kyn/configurationserver/KynConfigurationServerApplication.java
@@ -0,0 +1,15 @@
+package com.wittgroup.kyn.configurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class KynConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KynConfigurationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wittgroup/kyn/configurationserver/KynKynConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wittgroup.kyn.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KynKynConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
